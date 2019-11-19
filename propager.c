ba53e44a5fe4680ee24bbced23c5140d0daf5b0a
Fuji <ardjoune.nawfoel@outlook.fr>
2019-11-19 11:38:26 +0100

    Fonction traitement propager et compar en cours

---
+++ propager.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+/* Ordre de traitement G*/
+int propager(image g, int tab, int num_tache){
+	int i;
+	g
+	for(i=0;i<4;i++){
+		if(tab[y*w+x]==0] && compar()){
+			tab[x+w*y]=num_tache;
+		}
+	}
+
+}
---
+++ traitement.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+/*dans le traitement je retranscrit l'image dans le tableau d'int avec une meme valeur pour tout les points d'une meme tache*/
+void traitement(image g){
+	int i;
+	int num_tache=0;
+	int tab [image.w*image.h];
+	for(i=0;i<(image.w)*(image.h);++i){
+		if(tab[i]!=0){
+			propager()/*a faire*/ 
+		}
+		num_tache++;
+	}
+}
