13d4946d4625ab78e25cb5954c8040abdb30c90e
playmaker <chaoleicai@gmail.com>
2019-11-19 10:33:37 +0000

    creation struct image pixel

--- image.c
+++ image.c
@@ -1,10 +1,11 @@
 #include "image.h"
 
 void create_image(){
-
+    int a = 0;
+    a++;
 }
 
 int main(int argc, char const *argv[]) {
-
+    create_image();
     return 0;
 }
--- image.h
+++ image.h
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+
+typedef struct pixel pixel;
+struct pixel{
+    int R;
+    int G;
+    int B;
+};
+
+typedef struct image image;
+struct image{
+    int * pixel;
+    int w;
+    int h;
+};
