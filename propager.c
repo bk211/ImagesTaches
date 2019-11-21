4920b088814f880e5570c9366f32d2c26d8f82f1
Fuji <ardjoune.nawfoel@outlook.fr>
2019-11-21 12:09:23 +0100

    le traitement fontionne mais pas le décompte des tache

--- propager.c
+++ propager.c
@@ -2,11 +2,10 @@
 #include<stdlib.h>
 void affiche_tab(image g,int tab[]);
 /* Ordre de traitement G*/
-void propager(image g, int tab[], int i, int num_tache, pixel color){
+int propager(image g, int tab[], int i, int num_tache, pixel color){
 	if(tab[i]!=0){
-		affiche_tab(g,tab);
-	}
-	if(comparer_pixel(color,g.tab[i])){
+		return 0;
+	}else if(comparer_pixel(color,g.tab[i])){
 			tab[i]=num_tache;
 			if((i+1)<=g.w){
 			    propager(g,tab,i+1,num_tache,color);
@@ -25,6 +24,8 @@ void propager(image g, int tab[], int i, int num_tache, pixel color){
 			    printf("4\n");
 			}
 	}
+	affiche_tab(g,tab);
+	return 0;
 }
 
 
